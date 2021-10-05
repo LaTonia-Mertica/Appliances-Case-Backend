2b811fd643192955a9adf0cf739909ce3eca31b6
La'Tonia Mertica <latoniamertica@gmail.com>
2021-10-05 18:00:13 -0400

    front and back ends talking

--- models/db.js
+++ models/db.js
@@ -1,11 +1,39 @@
+// const { Client } = require("pg");
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(
-  `postgres://latoniamertica@localhost:5432/appliances`,
-  {
+//CODE BELOW ALLOWS BACKEND TO TALK TO FRONTEND
+// let db;
+
+// if (process.env.DATABASE_URL) {
+//   db = new Client({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   });
+// } else {
+//   db = new Client({
+//     user: process.env.DATABASE_USER,
+//     database: process.env.DATABASE_NAME,
+//     password: process.env.DATABASE_PASSWORD,
+//     host: process.env.DATABASE_HOST,
+//   });
+// }
+
+// db.connect();
+//CODE ABOVE ALLOWS BACKEND TO TALK TO FRONTEND
+let db;
+if (process.env.DATABASE_URL) {
+  db = new Sequelize(process.env.DATABASE_URL, {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  db = new Sequelize(`postgres://latoniamertica@localhost:5432/appliances`, {
     logging: false,
-  }
-);
+  });
+}
 
 const Customer = require("./Customer")(db);
 const User = require("./User")(db);
--- server.js
+++ server.js
@@ -2,91 +2,95 @@ const server = require("express")();
 server.use(require("body-parser").json());
 server.use(require("cors")());
 
-// const addTimeMiddleware = (req, res, next) => {
-//   res.locals.requestTime = new Date();
-//   next();
-// };
-//server.use(addTimeMiddleWare);
+//COMMENT OUT BELOW SECTION TO RUN BUT WITHOUT ENDPOINTS
+const addTimeMiddleware = (req, res, next) => {
+  res.locals.requestTime = new Date();
+  next();
+};
+server.use(addTimeMiddleware);
 
-// const { db, Customer, User } = require("./models/db.js");
-// const Op = require("sequelize").Op;
+const { db, Customer, User } = require("./models/db.js");
+const Op = require("sequelize").Op;
 
-// const isLoggedInMiddleware = async (req, res, next) => {
-//   if (!req.headers.email || !req.headers.password) {
-//     res.send({ error: "Authentication Criteria Must Be Met!" });
-//   } else {
-//     const userDB = await User.findOne({ where: { email: req.headers.email } });
-//     if (!userDB) {
-//       res.send({ error: "No User Known to Exist at That Email Address!" });
-//     } else {
-//       if (userDB.password === req.headers.password) {
-//         res.locals.user = userDB;
-//         next();
-//       } else {
-//         res.send({ error: "Password Not a Match to User Account!" });
-//       }
-//     }
-//   }
-// };
+const isLoggedInMiddleware = async (req, res, next) => {
+  if (!req.headers.email || !req.headers.password) {
+    res.send({ error: "Authentication Criteria Must Be Met!" });
+  } else {
+    const userDB = await User.findOne({ where: { email: req.headers.email } });
+    if (!userDB) {
+      res.send({ error: "No User Known to Exist at That Email Address!" });
+    } else {
+      if (userDB.password === req.headers.password) {
+        res.locals.user = userDB;
+        next();
+      } else {
+        res.send({ error: "Password Not a Match to User Account!" });
+      }
+    }
+  }
+};
+//RE-ADD CODE SECTION ABOVE PRN
 
 server.get("/", (req, res) => {
   res.send({ hello: "world" });
 });
 
-// server.get("/customers/:pageNum", isLoggedInMiddleware, async (req, res) => {
-//   const page = parseInt(req.params.pageNum);
-//   if (page <= 0) {
-//     res.send({
-//       customers: await Customer.findAndCountAll({
-//         limit: 5,
-//       }),
-//     });
-//   } else {
-//     res.send({
-//       customers: await Customer.findAndCountAll({
-//         limit: 5,
-//         offset: 5 * (page - 1),
-//       }),
-//     });
-//   }
-// });
+//COMMENT OUT BELOW SECTION TO RUN BUT WITHOUT ENDPOINTS
+server.get("/customers/:pageNum", isLoggedInMiddleware, async (req, res) => {
+  const page = parseInt(req.params.pageNum);
+  if (page <= 0) {
+    res.send({
+      customers: await Customer.findAndCountAll({
+        limit: 5,
+      }),
+    });
+  } else {
+    res.send({
+      customers: await Customer.findAndCountAll({
+        limit: 5,
+        offset: 5 * (page - 1),
+      }),
+    });
+  }
+});
 
-// server.post("/customers", isLoggedInMiddleware, async (req, res) => {
-//   if (req.body.zipCode.length !== 5) {
-//     res.send({ error: "Zip Code Too Short!" });
-//   } else {
-//     await Customer.create(req.body);
-//     res.send({ customers: await Customer.findAll() });
-//   }
-// });
+server.post("/customers", isLoggedInMiddleware, async (req, res) => {
+  if (req.body.zipCode.length !== 5) {
+    res.send({ error: "Zip Code Too Short!" });
+  } else {
+    await Customer.create(req.body);
+    res.send({ customers: await Customer.findAll() });
+  }
+});
 
-// server.post(`/customersSearch`, async (req, res) => {
-//   res.send({
-//     customers: await Customer.findAll({
-//       where: {
-//         [Op.or]: {
-//           firstName: { [Op.iLike]: `%${req.body.searchQuery}%` },
-//           lastName: { [Op.iLike]: `%${req.body.searchQuery}%` },
-//         },
-//       },
-//     }),
-//   });
-// });
+server.post(`/customersSearch`, async (req, res) => {
+  res.send({
+    customers: await Customer.findAll({
+      where: {
+        [Op.or]: {
+          firstName: { [Op.iLike]: `%${req.body.searchQuery}%` },
+          lastName: { [Op.iLike]: `%${req.body.searchQuery}%` },
+        },
+      },
+    }),
+  });
+});
 
-// server.post(`/login`, async (req, res) => {
-//   const userDB = await User.findOne({ where: { email: req.headers.email } });
-//   console.log(userDB);
+server.post(`/login`, async (req, res) => {
+  const userDB = await User.findOne({ where: { email: req.headers.email } });
+  console.log(userDB);
 
-//   if (!userDB) {
-//     res.send({ error: "No User Known to Exist at That Email Address!" });
-//   } else {
-//     if (userDB.password === req.headers.password) {
-//       res.send({ success: true });
-//     } else {
-//       res.send({ error: "Password Not a Match to User Account!" });
-//     }
-//   }
-// });
+  if (!userDB) {
+    res.send({ error: "No User Known to Exist at That Email Address!" });
+  } else {
+    if (userDB.password === req.headers.password) {
+      res.send({ success: true });
+    } else {
+      res.send({ error: "Password Not a Match to User Account!" });
+    }
+  }
+});
+//RE-ADD CODE SECTION ABOVE PRN
 
 let port = process.env.PORT;
 if (!port) {
